396eee9bc25b00f3219eb2c49884f57661948d50
jlandeen <jlandeen@epimatic.com>
2014-07-31 19:08:03 -0400

    Starting new Wrapper COM Class

---
+++ TogglCOM/NAV2Toggl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using TogglApi;
+
+namespace NAV2Toggl
+{
+
+    #region Clients
+    [ComVisible(true)]
+    [Guid("C00DBD2A-7DEE-432F-8AE4-48661C97BD24"),InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IMCTogglUser
+    {
+
+    }
+    #endregion
+}
